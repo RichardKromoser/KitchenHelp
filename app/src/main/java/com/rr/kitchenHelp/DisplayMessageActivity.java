0db8997d1ce88959723c76392028efbbd4b1dd71
RichardKromoser <richard.kromoser@student.uibk.ac.at>
2020-04-19 20:00:00 +0200

    removed add recipe in camera and display message activity

--- app/src/main/java/com/rr/kitchenHelp/DisplayMessageActivity.java
+++ app/src/main/java/com/rr/kitchenHelp/DisplayMessageActivity.java
@@ -11,6 +11,7 @@ public class DisplayMessageActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
         initializeDefaultToolbar();
+        setAddRecipeVisible(false);
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
